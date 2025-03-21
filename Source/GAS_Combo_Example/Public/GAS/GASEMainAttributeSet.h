6d40e2ba0d11c297e7fce2757c1a0121d8b2ed73
Dmitry Shilin <dshilin052@gmail.com>
2025-03-21 19:25:15 +0400

    DT update

--- Source/GAS_Combo_Example/Public/GAS/GASEMainAttributeSet.h
+++ Source/GAS_Combo_Example/Public/GAS/GASEMainAttributeSet.h
@@ -2,14 +2,31 @@
 
 #pragma once
 
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "CoreMinimal.h"
-#include "AbilitySystemComponent.h"
 
 #include "GASEMainAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)                                                                   \
+    GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName)                                                         \
+    GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)                                                                       \
+    GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)                                                                       \
+    GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 UCLASS()
 class GAS_COMBO_EXAMPLE_API UGASEMainAttributeSet : public UAttributeSet
 {
     GENERATED_BODY()
+
+  public:
+    ATTRIBUTE_ACCESSORS(UGASEMainAttributeSet, Health);
+    ATTRIBUTE_ACCESSORS(UGASEMainAttributeSet, MaxHealth);
+
+  protected:
+    UPROPERTY(BlueprintReadOnly, Category = "Main attributes")
+    FGameplayAttributeData Health;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Main attributes")
+    FGameplayAttributeData MaxHealth;
 };

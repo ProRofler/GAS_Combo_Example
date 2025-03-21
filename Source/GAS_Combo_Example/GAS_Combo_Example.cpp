89fc56537e90a343696d70ae709350b81e77c915
Dmitry Shilin <dshilin052@gmail.com>
2025-03-21 14:58:40 +0400

    Initial commit

---
+++ Source/GAS_Combo_Example/GAS_Combo_Example.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_Combo_Example.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_Combo_Example, "GAS_Combo_Example" );
